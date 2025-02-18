c4b2e6f1583b43b7afd44f36f093c2bb035b9aea
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-02-18 16:50:40 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import bgimg from "../assets/R.jpeg";
+import bgimg from "../assets/bgimg.jpg";
 
 const Home = () => {
   return (
-    <div className="home">
+    <div>
       <div
         className={`bg-[url(${bgimg})] bg-top bg-no-repeat bg-cover h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-4 text-center`}
       >
