f6c6b5d51a9a3dcbeb649dd73f64a61e4d564884
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-02-18 18:40:11 +0530

    Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div>
       <div
-        className={`img h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-6 text-center`}
+        className={`img h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-6 text-center `}
       >
-        <h1 className="text-5xl">Cats Make Life Better</h1>
-        <p className="text-xl text-gray-900">
+        <h1 className="text-5xl text-white">Cats Make Life Better</h1>
+        <p className="text-xl text-white ">
           The key to a happy life if having a cat
         </p>
         <button className="px-5 py-3 bg-blue-600 hover:bg-blue-700 text-white text-xl rounded-md font-semibold cursor-pointer transition-all">
