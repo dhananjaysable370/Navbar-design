6eae9a9e9a791c3f00c3fd9a31e2bead2c7b5e10
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-02-18 16:25:05 +0530

    completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import bg from '../assets/R.jpeg'
+import bg from "../assets/R.jpeg";
 const Home = () => {
   return (
     <div className="home">
-      <div className={`bg-${bg} bg-top bg-no-repeat bg-cover h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-4 text-center`}>
+      <div
+        className={`bg-[${bg}] bg-top bg-no-repeat bg-cover h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-4 text-center`}
+      >
         <h1 className="text-5xl">Cats Make Life Better</h1>
         <p className="text-xl text-gray-900">
           The key to a happy life if having a cat
