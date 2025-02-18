305a6c29f7664ea5cf09483a0cd4f67794c420a9
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-02-18 16:46:43 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import bgimg from '../assets/R.jpeg';
+import bgimg from "../assets/R.jpeg";
 
 const Home = () => {
   return (
     <div className="home">
       <div
-        className={`bg-[url('${bgimg}')] bg-top bg-no-repeat bg-cover h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-4 text-center`}
+        className={`bg-[url(${bgimg})] bg-top bg-no-repeat bg-cover h-[80vh] absolute top-24 left-0 w-[100vw] flex items-center justify-center bg-[rgba(12,8,8,0.15)] flex-col gap-4 text-center`}
       >
         <h1 className="text-5xl">Cats Make Life Better</h1>
         <p className="text-xl text-gray-900">
